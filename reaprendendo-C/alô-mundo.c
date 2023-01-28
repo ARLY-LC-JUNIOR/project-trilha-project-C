023f776e44f60b09ab178a26373ddecc90e79cc0
Arlyjr <arly.lcj@gmail.com>
2023-01-27 22:56:29 -0300

    fonte colorida no terminal

--- reaprendendo-C/alô-mundo.c
+++ reaprendendo-C/alô-mundo.c
@@ -7,7 +7,7 @@ int main(int argc, char * argv[] )
 {
     //A linha abaixo configura o idioma para portugues. 
     setlocale(LC_ALL, "Portuguese_Brazil");
-    system("color 7");
-    printf("Olá, alô, mundo, Hello, World...agora é backend e baixo nível com C ");
+    system("color D");
+    printf("...Olá alô mundo, Hello World...agora é backend e baixo nível com C ");
     return 0;
 }
