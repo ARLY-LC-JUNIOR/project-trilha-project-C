60d9d61a1f5f14f98e15c9aff7d5806718ebff6a
Arlyjr <arly.lcj@gmail.com>
2023-01-27 23:08:35 -0300

    linha limpa tela e print tela

--- reaprendendo-C/alô-mundo.c
+++ reaprendendo-C/alô-mundo.c
@@ -7,7 +7,13 @@ int main(int argc, char * argv[] )
 {
     //A linha abaixo configura o idioma para portugues. 
     setlocale(LC_ALL, "Portuguese_Brazil");
+    //Essa linha abaixo muda  cor da fonte no compilador/terminal deposi de executar. 
     system("color D");
-    printf("...Olá alô mundo, Hello World...agora é backend e baixo nível com C ");
+    printf("...Hello World, Olá alô mundo, agora é backend e com baixo nível em C ");
+    system ("pause");
+    //linha baixo limpa a tela 
+    system ("cls");
+    //Após aparecer o Hello world, aperte qualquer tecla, irá aparecer um novo print. 
+    printf("A vida é louca\n");
     return 0;
 }
